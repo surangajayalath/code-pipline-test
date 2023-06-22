c7d26a70f9adf095fc6eefc2e41c585aa9e5d7b3
M.Suranga Jayalath <56903228+surangajayalath@users.noreply.github.com>
2023-06-22 23:53:22 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,8 @@
-/**
- * @constructor
- */
 function MyClass() {
   this.foo = 'bar';
 }
 
-var someClass = function(){
-  this.prop = 1;
-}
+var someClass = 1;
 
-var obj1 = new someClass;  // Compliant
-var obj2 = new MyClass();  // Compliant regardless of considerJSDoc value
+var obj1 = new someClass;    // Noncompliant;
+var obj2 = new MyClass();    // Noncompliant if considerJSDoc parameter set to true. Compliant when considerJSDoc=false
