818e003c9fbefb2346eb329e327058bc81108704
M.Suranga Jayalath <56903228+surangajayalath@users.noreply.github.com>
2023-06-22 17:50:02 +0530

    Update bug.js

--- bug.js
+++ bug.js
@@ -6,3 +6,20 @@ async function bar() {
   // ...
   return await foo(); // Noncompliant
 }
+
+
+function someFunc(x) {
+  return typeof x === "Number"; // Noncompliant, function will always return 'false'
+}
+
+var x = 'a';
+
+var foo = {
+  y: 1
+}
+
+with (foo) {  // Noncompliant
+  y = 4;  // updates foo.y
+  x = 3;  // does NOT add a foo.x property; updates x var in outer scope
+}
+print(foo.x + " " + x); // shows: undefined 3
