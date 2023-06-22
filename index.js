2b1c26546d4f04c342905f08b3a1a2721a68c3da
M.Suranga Jayalath <56903228+surangajayalath@users.noreply.github.com>
2023-06-23 00:04:49 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,6 @@
-function MyClass() {
-  this.foo = 'bar';
-}
-
-var someClass = 1;
-
-var obj1 = new someClass;    // Noncompliant;
-var obj2 = new MyClass();    // Noncompliant if considerJSDoc parameter set to true. Compliant when considerJSDoc=false
-
-
-if (var == 'howdy') {...} // Noncompliant
-if (var == 'howdy') {...} // Noncompliant
-if (var == 'howdy') {...} // Noncompliant
-if (var == 'howdy') {...} // Noncompliant
-if (var == 'howdy') {...} // Noncompliant
+if (var === 'howdy') {...}
+if (var === 'howdy') {...}
+if (var === 'howdy') {...}
+if (var === 'howdy') {...}
+if (var === 'howdy') {...}
+if (var === 'howdy') {...}
