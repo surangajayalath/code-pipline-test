162fd419581c740eae6840524d021eefb4999956
M.Suranga Jayalath <56903228+surangajayalath@users.noreply.github.com>
2023-06-22 17:04:22 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,46 @@
-// the hello world program
-console.log('Hello World');
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Login Page</title>
+</head>
+<body>
+  <h1>Login</h1>
+  <form id="loginForm">
+    <div>
+      <label for="username">Username:</label>
+      <input type="text" id="username" name="username" required>
+    </div>
+    <div>
+      <label for="password">Password:</label>
+      <input type="password" id="password" name="password" required>
+    </div>
+    <div>
+      <button type="submit">Login</button>
+    </div>
+  </form>
 
-<?php
-echo "hell"
-?>
+  <script>
+    document.getElementById('loginForm').addEventListener('submit', function(event) {
+      event.preventDefault();
+      const username = document.getElementById('username').value;
+      const password = document.getElementById('password').value;
+
+      // Simulate API call for authentication
+      authenticateUser(username, password);
+    });
+
+    function authenticateUser(username, password) {
+      if (username === 'admin' && password === 'admin123') {
+        alert('Login successful!');
+        // Redirect to home page
+        window.location.href = '/home.html';
+      } else {
+        alert('Invalid credentials. Please try again.');
+      }
+    }
+
+    // BUG: Password is logged to the console, potentially exposing sensitive information
+    console.log('Password: ' + document.getElementById('password').value);
+  </script>
+</body>
+</html>
