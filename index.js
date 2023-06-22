112bd9b6aebd1165c9ca164b39718269e023de21
M.Suranga Jayalath <56903228+surangajayalath@users.noreply.github.com>
2023-06-23 00:10:55 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
-if (var === 'howdy') {...}
-if (var === 'howdy') {...}
-if (var === 'howdy') {...}
-if (var === 'howdy') {...}
-if (var === 'howdy') {...}
-if (var === 'howdy') {...}
+const jwt = require('jsonwebtoken');
+
+let token = jwt.sign({ foo: 'bar' }, key, { algorithm: 'HS256' }); // Compliant
+
+jwt.verify(token, key, { expiresIn: 360000 * 5, algorithms: ['HS256'] }, callbackcheck); // Compliant
+
+
+const jwt = require('jsonwebtoken');
+
+let token = jwt.sign({ foo: 'bar' }, key, { algorithm: 'none' }); // Noncompliant: 'none' cipher doesn't sign the JWT (no signature will be included)
+
+jwt.verify(token, key, { expiresIn: 360000 * 5, algorithms: ['RS256', 'none'] }, callbackcheck); // Noncompliant: 'none' cipher should not be used when verifying JWT signature
