c6abe9fcd35112bd16a92dda0904abb355ec7d11
M.Suranga Jayalath <56903228+surangajayalath@users.noreply.github.com>
2023-06-23 22:55:39 +0530

    Update index.js

--- index.js
+++ index.js
@@ -69,3 +69,37 @@ switch (day) {
     break;
   ...
 }
+
+/**
+ * @constructor
+ */
+function MyClass() {
+  this.foo = 'bar';
+}
+
+var someClass = function(){
+  this.prop = 1;
+}
+
+var obj1 = new someClass;  // Compliant
+var obj2 = new MyClass();  // Compliant regardless of considerJSDoc value
+
+
+
+/**
+ * @constructor
+ */
+function MyClass() {
+  this.foo = 'bar';
+}
+
+var someClass = function(){
+  this.prop = 1;
+}
+
+var obj1 = new someClass;  // Compliant
+var obj2 = new MyClass();  // Compliant regardless of considerJSDoc value
+
+
+
+
