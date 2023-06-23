6130bc122a537a8739472aa3a6bc993d641c6318
M.Suranga Jayalath <56903228+surangajayalath@users.noreply.github.com>
2023-06-23 11:00:43 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
-const jwt = require('jsonwebtoken');
+// const jwt = require('jsonwebtoken');
 
-let token = jwt.sign({ foo: 'bar' }, key, { algorithm: 'HS256' }); // Compliant
+// let token = jwt.sign({ foo: 'bar' }, key, { algorithm: 'HS256' }); // Compliant
 
-jwt.verify(token, key, { expiresIn: 360000 * 5, algorithms: ['HS256'] }, callbackcheck); // Compliant
+// jwt.verify(token, key, { expiresIn: 360000 * 5, algorithms: ['HS256'] }, callbackcheck); // Compliant
 
 
-const jwt = require('jsonwebtoken');
+// const jwt = require('jsonwebtoken');
 
-let token = jwt.sign({ foo: 'bar' }, key, { algorithm: 'none' }); // Noncompliant: 'none' cipher doesn't sign the JWT (no signature will be included)
+// let token = jwt.sign({ foo: 'bar' }, key, { algorithm: 'none' }); // Noncompliant: 'none' cipher doesn't sign the JWT (no signature will be included)
 
-jwt.verify(token, key, { expiresIn: 360000 * 5, algorithms: ['RS256', 'none'] }, callbackcheck); // Noncompliant: 'none' cipher should not be used when verifying JWT signature
+// jwt.verify(token, key, { expiresIn: 360000 * 5, algorithms: ['RS256', 'none'] }, callbackcheck); // Noncompliant: 'none' cipher should not be used when verifying JWT signature
+
+
+if (x == 0) {
+  doSomething();
+} else if (x == 1) {
+  doSomethingElse();
+} else {
+  throw "Unexpected value for x";
+}
