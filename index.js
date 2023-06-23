8c629db350f6007843214e6b270ded514ae34ced
M.Suranga Jayalath <56903228+surangajayalath@users.noreply.github.com>
2023-06-23 23:06:06 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,105 +1,12 @@
-const sym = new Symbol("foo");  
-const sym = new Symbol("foo");  
-const sym = new Symbol("foo");  
-const sym = new Symbol("foo");  
-const sym = new Symbol("foo");  
-const sym = new Symbol("foo");  
 
-switch (day) {
-  case MONDAY:
-  case TUESDAY:
-  WEDNESDAY:   // instead of "case WEDNESDAY"
-    doSomething();
-    break;
-  ...
+void doSomething();
+
+
+class Dog extends Animal {
+  constructor(name) {
+    super();
+    this.name = name;
+    super();         // Noncompliant
+    super.doSomething();
+  }
 }
-switch (day) {
-  case MONDAY:
-  case TUESDAY:
-  WEDNESDAY:   // instead of "case WEDNESDAY"
-    doSomething();
-    break;
-  ...
-}
-switch (day) {
-  case MONDAY:
-  case TUESDAY:
-  WEDNESDAY:   // instead of "case WEDNESDAY"
-    doSomething();
-    break;
-  ...
-}
-switch (day) {
-  case MONDAY:
-  case TUESDAY:
-  WEDNESDAY:   // instead of "case WEDNESDAY"
-    doSomething();
-    break;
-  ...
-}
-switch (day) {
-  case MONDAY:
-  case TUESDAY:
-  WEDNESDAY:   // instead of "case WEDNESDAY"
-    doSomething();
-    break;
-  ...
-}
-switch (day) {
-  case MONDAY:
-  case TUESDAY:
-  WEDNESDAY:   // instead of "case WEDNESDAY"
-    doSomething();
-    break;
-  ...
-}
-v
-switch (day) {
-  case MONDAY:
-  case TUESDAY:
-  WEDNESDAY:   // instead of "case WEDNESDAY"
-    doSomething();
-    break;
-  ...
-}switch (day) {
-  case MONDAY:
-  case TUESDAY:
-  WEDNESDAY:   // instead of "case WEDNESDAY"
-    doSomething();
-    break;
-  ...
-}
-
-/**
- * @constructor
- */
-function MyClass() {
-  this.foo = 'bar';
-}
-
-var someClass = function(){
-  this.prop = 1;
-}
-
-var obj1 = new someClass;  // Compliant
-var obj2 = new MyClass();  // Compliant regardless of considerJSDoc value
-
-
-
-/**
- * @constructor
- */
-function MyClass() {
-  this.foo = 'bar';
-}
-
-var someClass = function(){
-  this.prop = 1;
-}
-
-var obj1 = new someClass;  // Compliant
-var obj2 = new MyClass();  // Compliant regardless of considerJSDoc value
-
-
-
-
